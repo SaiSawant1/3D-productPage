a7e4c243b39015ac3c3e8a4d2dfe1815b50702e8
Sai Sawant <saisawant2003@gmail.com>
2023-07-13 23:54:14 +0530

    bug fixes

--- src/components/CameraRig.tsx
+++ src/components/CameraRig.tsx
@@ -5,10 +5,10 @@ import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import state from "@/store";
 import { REPLCommand } from "repl";
-import { Vector3 } from "three";
+import { Group, Vector3 } from "three";
 import {gsap} from "gsap"
 const CameraRig = ({ children }: { children: React.ReactNode }) => {
-  const group = React.useRef(null);
+  const group = React.useRef<Group | null>(null);
   const snap = useSnapshot(state);
   useFrame((state, delta) => {
     const isBreakpoint=window.innerWidth<=1200
@@ -26,12 +26,14 @@ const CameraRig = ({ children }: { children: React.ReactNode }) => {
     easing.damp3(state.camera.position, targetPosition, 0.25, delta)
 
     // set the model rotation smoothly
-    easing.dampE(
-      group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x / 5, 0],
-      0.25,
-      delta
-    )
+    if (group.current !== null) { // Add null check
+      easing.dampE(
+        group.current.rotation,
+        [state.pointer.y / 10, -state.pointer.x / 5, 0],
+        0.25,
+        delta
+      );
+    }
   });
 
   return <group ref={group}>{children}</group>;
